64bb4f30943e148b2db86eaa03dcc33377799a87
Pedro Nunes <99264609+nunes262@users.noreply.github.com>
2024-04-23 20:13:49 -0300

    Update Atendente.java

--- Atividade Avaliativa (3pts) Diagrama de Classe e Código Java/src/classes/Atendente.java
+++ Atividade Avaliativa (3pts) Diagrama de Classe e Código Java/src/classes/Atendente.java
@@ -1,14 +1,13 @@
 package classes;
 
 public class Atendente {
-
     private String nome;
 
     public Atendente(String nome) {
         this.nome = nome;
     }
 
-    public Resposta atenderSolicitacao(Solicitação solicitacao) {
+    public Resposta atenderSolicitação(Solicitação solicitacao) {
         // Processar a solicitação e gerar uma resposta
         return new Resposta("Seu problema foi resolvido.");
     }
