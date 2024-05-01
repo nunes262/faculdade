1d165da7fea6e1d91af8103b7fa29153ed493fa3
nunes262 <pno.mco.jcn@gmail.com>
2024-05-01 11:17:24 -0300

    corrections

--- Exercícios de Repetição/exercicio28.java
+++ Exercícios de Repetição/exercicio28.java
@@ -1,8 +1,8 @@
 public class exercicio28 {
     public static void main(String[] args) {
         System.out.println("Começando a contagem: ");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i + 1);
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
         }
         System.out.println("Fim da contagem");
     }
