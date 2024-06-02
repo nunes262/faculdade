7c5471ec86ae2bfb264c2a879f99efe76356a032
nunes262 <pno.mco.jcn@gmail.com>
2024-06-02 17:40:15 -0300

    project vida&carreira

---
+++ vida-carreira/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Carousel from "react-elastic-carousel";
+import Item from "./Item";
+import "./App.css";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 }
+];
+
+function App() {
+  const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+
+
+  return (
+    <div className="App">
+      <hr className="seperator" />
+      <div className="carousel-wrapper">
+        <Carousel breakPoints={breakPoints}>
+          {items.map((item) => (
+            <Item key={item}>{item}</Item>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
+}
+
+export default App;
